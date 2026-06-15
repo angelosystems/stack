0616effb64703d9e434f25e775e0059195f8aa95
obsidian <276724264+angelosystems@users.noreply.github.com>
2026-06-15 16:51:22 +0000

    feat: implement Deploy-on-Merge-Reactor (st-v79v)

--- tools/portfolio/master-kanban/main.go
+++ tools/portfolio/master-kanban/main.go
@@ -147,7 +147,7 @@ func main() {
 	}
 	root.PersistentFlags().StringVar(&dsn, "dsn", envOr("PORTFOLIO_DSN", "postgres://mario:c8f2b7025f25a3fa9149c4fb4e20cc18@127.0.0.1:5434/mario_brain?sslmode=disable"), "Postgres DSN")
 
-	root.AddCommand(cmdList(), cmdAdd(), cmdMove(), cmdLink(), cmdSync(), cmdServe(), cmdEvents(), cmdResolveRepo())
+	root.AddCommand(cmdList(), cmdAdd(), cmdMove(), cmdLink(), cmdSync(), cmdServe(), cmdEvents(), cmdResolveRepo(), cmdDeployReactor())
 
 	if err := root.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
